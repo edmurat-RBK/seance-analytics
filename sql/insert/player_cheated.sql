cbea30b4e715993736f3d59661e406de8ce81061
Edouard MURAT <e.murat@rubika-edu.com>
2023-01-05 14:28:55 +0100

    fix: field name error in sql script

--- sql/insert/player_cheated.sql
+++ sql/insert/player_cheated.sql
@@ -1,4 +1,4 @@
-INSERT INTO player_cheated(session_uuid, game_uuid, chapter_index, turn_index, c)
+INSERT INTO player_cheated(session_uuid, game_uuid, chapter_index, turn_index, cheat_type)
 VALUES (
     UUID_TO_BIN("{sessionUuid}"),
     UUID_TO_BIN("{gameUuid}"),
